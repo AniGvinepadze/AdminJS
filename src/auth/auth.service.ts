efa58a01aa739dc853873a863aae4188631c632f
Ani <a.gvin3@gmail.com>
2025-05-14 15:46:12 +0400

    Feat ( Auth ) : Sign-Up

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,28 @@
-import { Injectable } from '@nestjs/common';
-
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Admin } from 'src/admin/schema/admin.schema';
+import { SignUpDto } from './dto/sign-up.dto';
+import * as bcrypt from "bcrypt"
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(
+        @InjectModel('admin') private readonly adminModel: Model<Admin>,
+        private jwtService:JwtService
+     ){}
+
+    async signUp({email,fullName,password}:SignUpDto){
+        const existUser = await this.adminModel.findOne({email})
+        if(existUser) throw new BadRequestException("Admin already exists")
+
+        const hashedPassword = await bcrypt.hash(password,10)
+
+        await this.adminModel.create({email,password:hashedPassword,fullName})
+
+        return "user registered successfully"
+    }
+    
+
+
+}
