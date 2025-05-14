92239e3e235abb774aebd8445894e8d8ea4a2174
Ani <a.gvin3@gmail.com>
2025-05-14 16:35:02 +0400

    Feat ( Auth ) : Sing-in

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,25 +4,42 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Admin } from 'src/admin/schema/admin.schema';
 import { SignUpDto } from './dto/sign-up.dto';
-import * as bcrypt from "bcrypt"
+import * as bcrypt from 'bcrypt';
+import { SignInDto } from './dto/sign-in.dto';
 @Injectable()
 export class AuthService {
-    constructor(
-        @InjectModel('admin') private readonly adminModel: Model<Admin>,
-        private jwtService:JwtService
-     ){}
+  constructor(
+    @InjectModel('admin') private readonly adminModel: Model<Admin>,
+    private jwtService: JwtService,
+  ) {}
 
-    async signUp({email,fullName,password}:SignUpDto){
-        const existUser = await this.adminModel.findOne({email})
-        if(existUser) throw new BadRequestException("Admin already exists")
+  async signUp({ email, fullName, password }: SignUpDto) {
+    const existUser = await this.adminModel.findOne({ email });
+    if (existUser) throw new BadRequestException('Admin already exists');
 
-        const hashedPassword = await bcrypt.hash(password,10)
+    const hashedPassword = await bcrypt.hash(password, 10);
 
-        await this.adminModel.create({email,password:hashedPassword,fullName})
+    await this.adminModel.create({ email, password: hashedPassword, fullName });
 
-        return "user registered successfully"
-    }
-    
+    return 'user registered successfully';
+  }
 
+  async signIn({ email, password }: SignInDto) {
+    const existUser = await this.adminModel.findOne({ email });
+    if (!existUser)
+      throw new BadRequestException('Email or Password is invalid');
 
+    const isPasswordEqual = await bcrypt.compare(password, existUser.password);
+    if (!isPasswordEqual)
+      throw new BadRequestException('Email or Password is invalid');
+
+    const payLoad = {
+      userId: existUser._id,
+    };
+
+    const accessToken = await this.jwtService.sign(payLoad, {
+      expiresIn: '1h',
+    });
+    return { accessToken };
+  }
 }
