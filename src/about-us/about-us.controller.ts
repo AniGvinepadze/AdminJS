29a283d8c2504ffa8e0c25399943f58dcca7275d
Ani <a.gvin3@gmail.com>
2025-05-14 21:07:51 +0400

    Fix ( Guards ) : isAuthGuard

--- src/about-us/about-us.controller.ts
+++ src/about-us/about-us.controller.ts
@@ -1,13 +1,24 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseGuards,
+} from '@nestjs/common';
 import { AboutUsService } from './about-us.service';
 import { CreateAboutUsDto } from './dto/create-about-us.dto';
 import { UpdateAboutUsDto } from './dto/update-about-us.dto';
+import { isAuthGuard } from 'src/guards/auth.guard';
 
 @Controller('about-us')
 export class AboutUsController {
   constructor(private readonly aboutUsService: AboutUsService) {}
 
   @Post()
+  @UseGuards(isAuthGuard)
   create(@Body() createAboutUsDto: CreateAboutUsDto) {
     return this.aboutUsService.create(createAboutUsDto);
   }
@@ -19,16 +30,17 @@ export class AboutUsController {
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    
     return this.aboutUsService.findOne(id);
   }
 
   @Patch(':id')
+  @UseGuards(isAuthGuard)
   update(@Param('id') id: string, @Body() updateAboutUsDto: UpdateAboutUsDto) {
     return this.aboutUsService.update(id, updateAboutUsDto);
   }
 
   @Delete(':id')
+  @UseGuards(isAuthGuard)
   remove(@Param('id') id: string) {
     return this.aboutUsService.remove(id);
   }
--- src/feedback/feedback.controller.ts
+++ src/feedback/feedback.controller.ts
@@ -1,13 +1,24 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseGuards,
+} from '@nestjs/common';
 import { FeedbackService } from './feedback.service';
 import { CreateFeedbackDto } from './dto/create-feedback.dto';
 import { UpdateFeedbackDto } from './dto/update-feedback.dto';
+import { isAuthGuard } from 'src/guards/auth.guard';
 
 @Controller('feedback')
 export class FeedbackController {
   constructor(private readonly feedbackService: FeedbackService) {}
 
   @Post()
+  @UseGuards(isAuthGuard)
   create(@Body() createFeedbackDto: CreateFeedbackDto) {
     return this.feedbackService.create(createFeedbackDto);
   }
@@ -23,11 +34,16 @@ export class FeedbackController {
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateFeedbackDto: UpdateFeedbackDto) {
+  @UseGuards(isAuthGuard)
+  update(
+    @Param('id') id: string,
+    @Body() updateFeedbackDto: UpdateFeedbackDto,
+  ) {
     return this.feedbackService.update(id, updateFeedbackDto);
   }
 
   @Delete(':id')
+  @UseGuards(isAuthGuard)
   remove(@Param('id') id: string) {
     return this.feedbackService.remove(id);
   }
