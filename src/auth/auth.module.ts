4df72605de0626d9cf39fc3956bacd9883f41988
Ani <a.gvin3@gmail.com>
2025-05-14 15:55:06 +0400

    Update ( Auth ) : authmodule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { adminSchema } from 'src/admin/schema/admin.schema';
+import { JwtModule, JwtService } from '@nestjs/jwt';
 
 @Module({
+  imports:[
+    ConfigModule.forRoot(),
+    MongooseModule.forFeature([{name:"admin",schema:adminSchema}]),
+    JwtModule.register({
+      global:true,
+      secret:process.env.JWT_SECRET
+    })
+
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
