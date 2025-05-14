1805649a00ff550ed2745c12e6a64a7cb7ad23c5
Ani <a.gvin3@gmail.com>
2025-05-15 00:25:31 +0400

    Fix ( FAQ ) : add isAuthGuard

--- src/faq/faq.controller.ts
+++ src/faq/faq.controller.ts
@@ -1,13 +1,24 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseGuards,
+} from '@nestjs/common';
 import { FaqService } from './faq.service';
 import { CreateFaqDto } from './dto/create-faq.dto';
 import { UpdateFaqDto } from './dto/update-faq.dto';
+import { isAuthGuard } from 'src/guards/auth.guard';
 
 @Controller('faq')
 export class FaqController {
   constructor(private readonly faqService: FaqService) {}
 
   @Post()
+  @UseGuards(isAuthGuard)
   create(@Body() createFaqDto: CreateFaqDto) {
     return this.faqService.create(createFaqDto);
   }
@@ -23,11 +34,13 @@ export class FaqController {
   }
 
   @Patch(':id')
+  @UseGuards(isAuthGuard)
   update(@Param('id') id: string, @Body() updateFaqDto: UpdateFaqDto) {
     return this.faqService.update(id, updateFaqDto);
   }
 
   @Delete(':id')
+  @UseGuards(isAuthGuard)
   remove(@Param('id') id: string) {
     return this.faqService.remove(id);
   }
