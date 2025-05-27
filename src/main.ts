09a6bbf56f75182c6cb5a396e6d9dd672f879d7a
Ani <a.gvin3@gmail.com>
2025-05-27 19:17:10 +0400

    push push

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: ['https://adminpanel-reeducate-jkl1.vercel.app/','http://localhost:3001/'],
-    credentials:true
-  });
+  app.enableCors();
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
