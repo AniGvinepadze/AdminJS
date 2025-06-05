4f7204056f598b18ae27c834154b4600ca6aca2d
Ani <a.gvin3@gmail.com>
2025-06-05 15:01:51 +0400

    add ( CreateCourseDto ) :  add all course details page fields

--- src/courses/dto/create-course.dto.ts
+++ src/courses/dto/create-course.dto.ts
@@ -3,14 +3,55 @@ import { IsArray, IsNotEmpty, IsOptional, IsString } from 'class-validator';
 export class CreateCourseDto {
   @IsNotEmpty()
   @IsString()
+  @IsOptional()
   name: string;
 
   @IsArray()
   @IsOptional()
   images?: string[];
-  
 
   @IsNotEmpty()
   @IsString()
+  @IsOptional()
   category: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @IsOptional()
+  courseTitle: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @IsOptional()
+  courseDuration: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @IsOptional()
+  courseQuantity: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @IsOptional()
+  coursePrice: string;
+  @IsNotEmpty()
+  @IsString()
+  @IsOptional()
+  courseGoal: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @IsOptional()
+  CourseDestription: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @IsOptional()
+  courseLittleGoals: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @IsOptional()
+  courseSyllabus: string;
+
 }
