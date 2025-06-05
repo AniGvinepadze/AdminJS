946b68f201098ce9af7071cbc929d64c34a4fdd0
Ani <a.gvin3@gmail.com>
2025-06-05 14:56:39 +0400

    Add ( Schema ) : add course details page information

--- src/courses/schema/course.schema.ts
+++ src/courses/schema/course.schema.ts
@@ -8,11 +8,34 @@ export class Course {
 
   @Prop({ type: [String], default: [] })
   images: string[];
-  
-  
 
   @Prop({ type: String })
   category: string;
+
+  @Prop({ type: String })
+  courseTitle: string;
+
+  @Prop({ type: String })
+  courseDuration: string;
+
+  @Prop({ type: String })
+  courseQuantity: string;
+
+  @Prop({ type: String })
+  coursePrice: string;
+
+  @Prop({ type: String })
+  courseGoal: string;
+
+  @Prop({ type: String })
+  CourseDestription: string;
+
+  @Prop({ type: String })
+  courseLittleGoals: string;
+
+  @Prop({ type: String })
+  courseSyllabus: string;
+
 }
 
 export const courseSchema = SchemaFactory.createForClass(Course);
